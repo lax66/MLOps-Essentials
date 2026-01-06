44d420f9ddedcf386b878f3ef9dcd75d34b7e437
Ubuntu <azureuser@Serving-ML-model.hvevzz0gzidedlzjsvy5p5pr4f.bx.internal.cloudapp.net>
2026-01-06 12:17:42 +0000

    add crop-yeild dataset and train files

---
+++ 5-Model-Deployment-Serving-using-VM/3-Serve-in-Cloud-VM-EC2/crop-yeild-ml/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+
+app = FastAPI(title="Crop Yield Prediction API")
+
+pipeline = joblib.load("model/pipeline.pkl")
+
+@app.post("/predict")
+def predict(data: dict):
+    df = pd.DataFrame([data])
+    prediction = pipeline.predict(df)[0]
+    return {
+        "predicted_yield_ton_per_hectare": round(float(prediction), 2)
+    }
+
